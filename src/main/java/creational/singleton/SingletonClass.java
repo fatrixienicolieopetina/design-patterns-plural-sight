0adfa10e8e5567df5ae7c2ab351bb07137490ce6
Patricia Nicole Opetina <patricianicole.opetina@gmail.com>
2022-07-24 21:49:33 +0800

    singleton pattern

--- src/main/java/creational/singleton/SingletonClass.java
+++ src/main/java/creational/singleton/SingletonClass.java
@@ -2,30 +2,33 @@ package creational.singleton;
 
 public class SingletonClass {
 	/*
-	 * The keyword volatile helps in the thread safety. It ensures that
-	 * the value will always be fetched from the main memory
+	 * The keyword volatile helps for thread safety. It ensures that the value will
+	 * always be fetched from the main memory
 	 */
 	private static volatile SingletonClass instance = null;
-	
-	private SingletonClass() {	
+
+	private SingletonClass() {
 		/*
-		 * A runtime exception is thrown if reflection is used 
-		 * to instantiate this class
+		 * A runtime exception is thrown if reflection is used to instantiate this class
 		 */
 		if (instance != null) {
 			throw new RuntimeException("Use getInstance() method to instantiate the class");
 		}
 	}
-	
+
 	public static SingletonClass getInstance() {
+		/*
+		 * instantiation happens during the first call on get instance for lazy-loading
+		 */
 		if (instance == null) {
+			// synchronized keyword is also used for thread safety
 			synchronized (SingletonClass.class) {
 				if (instance == null) {
 					instance = new SingletonClass();
 				}
 			}
 		}
-		
+
 		return instance;
 	}
 }
--- src/main/java/creational/singleton/SingletonPattern.java
+++ src/main/java/creational/singleton/SingletonPattern.java
@@ -3,8 +3,17 @@ package creational.singleton;
 public class SingletonPattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * A singleton should be used to guarantee one instance of a class. It is easy
+		 * to implement.
+		 * 
+		 * Common pitfalls of a singleton pattern include 
+		 * - often over-used 
+		 * - difficult to unit test 
+		 * - if not careful, is not thread safe
+		 */
+		var singletonClass = SingletonClass.getInstance();
+		System.out.print(singletonClass);
 	}
 
 }
